db69113ef86ae3dddb58d8d1cde3f5a7311fec08
FourteenBrush <naessensarthur2@protonmail.com>
2023-11-27 16:37:58 +0100

    Add batch build script

---
+++ build.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set COLLECTIONS="reader=dependencies/Classreader/src"
+set FLAGS=-strict-style -vet-unused
+set OUT_NAME=scratch
+
+if "%1" == "debug" (
+    rem debug build
+    set EXTRA_FLAGS=-debug
+) else if "%1" == "run" (
+    shift
+    shift
+    rem set %OUT_NAME% to be args[0] for the program
+    odin run src -collection=%COLLECTIONS% %FLAGS% %EXTRA_FLAGS% -- %OUT_NAME% %*
+    exit
+) else ( 
+    rem release build
+    set EXTRA_FLAGS=-o:speed
+)
+
+odin build src -out:%OUT_NAME%.exe -collection=%COLLECTIONS% %FLAGS% %EXTRA_FLAGS%
